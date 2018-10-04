855e5097c13951242f478b1a74816deb9942df99
clovis <clovis.rodriguez@rappi.com>
2018-10-04 15:15:30 -0500

    Searchbar & fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '711473173e34c42a9fea97d5092ef7e2';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},co`;
+  const request = axios.get(url);
+  console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
-    this.onInputChange = this.onInputChange.bind(this)
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
     this.state = { term: ''};
   }
 
@@ -13,6 +17,8 @@ export default class SearchBar extends Component {
 
   onFormSubmit(event){
     event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: ''});
   }
 
   render() {
@@ -22,8 +28,7 @@ export default class SearchBar extends Component {
           placeholder ="Get a five-day forecast in your favorite cities"
           className="form-control"
           value={this.state.term}
-          onChange={this.onInputChange}
-           />
+          onChange={this.onInputChange} />
         <span className ="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
@@ -31,3 +36,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
